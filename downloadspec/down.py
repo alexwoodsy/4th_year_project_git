bacf2ece52602fe251353628c2fcd5ae9cf38cd9
Alex Woods <tradgiik@gmail.com>
2020-02-13 11:35:36 +0000

    intial code
    basic spectra downloading and continuum fitting

---
+++ downloadspec/down.py
@@ -0,0 +1,10 @@
+import wget
+
+f = open("downloadtest_url.txt", "r")
+fitsurl = f.read().split()
+fitsurl = tuple(fitsurl)
+f.close()
+
+for i in range(0,len(fitsurl)):
+    urldown = fitsurl[i]
+    wget.download(urldown, "downloads")
