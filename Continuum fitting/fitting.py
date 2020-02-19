98dbd77b62362328939e86868ef7b7fc7bc06407
Alex Woods <tradgiik@gmail.com>
2020-02-19 14:30:36 +0000

    added interploating method of hull fitting to subtract continuum

--- Continuum fitting/fitting.py
+++ Continuum fitting/fitting.py
@@ -2,18 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.modeling import models, fitting
 from astropy.io import fits
+from scipy.interpolate import interp1d
 import os
 
 specnames = next(os.walk('Spectra'))[2] #dir is your directory path as string
 spectot = len(specnames)
 
 #add indexing for epctra in file to allow loop over all
-i=10
+i=1
 specdirectory = 'Spectra/'+specnames[i]
 print(specdirectory)
 
 data = fits.getdata(specdirectory,ext=1)#import fits image
-wlim = 3000
+wlim = len(data)
 flux = np.zeros(len(data))
 wlen = np.zeros(len(data))
 model = np.zeros(len(data))
@@ -25,12 +26,37 @@ for i in range(0,len(data)):
  model[i] = data[i][7]
  wlen[i] = 10**(data[i][1])
 
+#ston calculation
 std = (ivar)**-0.5
 ston = np.median(flux/std)
 print("S/N ratio = ",ston)
 
+
+#cont fitting
+intervals = 30
+window = int(len(data)/intervals)
+step = 0
+i = 0
+intervalwlen = np.zeros(intervals)
+winpeak = np.zeros(intervals)
+
+while (step+window) <= len(data):
+    windata = flux[step:(step+window)]
+    winpeakind = step + np.argmax(windata)
+    winpeak[i] = np.max(windata)
+    intervalwlen[i] = wlen[winpeakind]
+    step = step + window
+    i = i + 1
+
+intpol = interp1d(intervalwlen, winpeak, kind='cubic')
+xnew = np.linspace(intervalwlen[0],intervalwlen[-1], num=100, endpoint=True)
+
+
+#plotting
+
 plt.plot(wlen[0:wlim],flux[0:wlim])
-#plt.plot(wlen[0:wlim],model[0:wlim])
+plt.plot(intervalwlen[0:wlim],winpeak[0:wlim],'*')
+plt.plot(xnew,intpol(xnew),'--')
 plt.xlabel('wavelength (Angstroms)')
 plt.ylabel('Flux')
 
