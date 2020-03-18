c6536e145b1c7864c7ffa34c271436e3035f9a37
alexwoodsy <61010135+alexwoodsy@users.noreply.github.com>
2020-03-18 15:40:57 +0000

    added stacking method test + stacking spectra using interpolation

--- stacking/stacking_V1_alex.py
+++ stacking/stacking_V1_alex.py
@@ -7,7 +7,7 @@ from scipy import interpolate
 import os
 #import fitting
 import sys
-sys.path.append('C:/Users/alexw/OneDrive/Documents/University work/4th year work/Main project/4th_year_project_git/Continuum fitting')
+sys.path.append('C:/Users/alexw/Documents/GitHub/4th_year_project_git/Continuum fitting')
 import fittingmethods as fitmeth
 
 #plt.style.use('mystyle') #path C:\Users\alexw\AppData\Local\Programs\Python\Python37\Lib\site-packages\matplotlib\mpl-data\stylelib
@@ -15,40 +15,49 @@ import fittingmethods as fitmeth
 #imports the spectra from the spectra folder
 specnames = next(os.walk('Spectra'))[2]
 spectot = len(specnames)
+
 specsample = np.array([0,2,1000])
 
 
+#test data
+x1 = np.arange(1,7,0.02)
+y1 = np.sin(x1)
+plt.plot(x1,y1)
+x2 = np.arange(1,9,0.05)
+y2 = np.sin(x2)
+plt.plot(x2,y2)
 
-fitsloc = 'stacking/contfitraw.fits'
+shift = 0 - np.max(y1)
+xshift = x1 + shift
+xhighres = np.linspace(np.min(xshift), np.max(xshift), 1000)
+xintpol = interpolate.interp1d(xshift, y1, 'linear')
+y1highres = xintpol(xhighres)
 
-col = fits.Column(name=specnames[0], array=np.array([1, 5]), format='K')
-tablehdu = fits.BinTableHDU.from_columns([col])
+shift = 0 - np.max(y2)
+xshift = x2 + shift
+xhighres = np.linspace(np.min(xshift), np.max(xshift), 1000)
+xintpol = interpolate.interp1d(xshift, y2, 'linear')
+y2highres = xintpol(xhighres)
 
-wlen = np.array([1,2,3,4,5,6])
-normspec = np.array([1,2,3,4,5,6])
-
-for specind in specsample[1:]:
-    data = wlen
-    col = fits.Column(name=specnames[specind], array=data, format='K')
-    tablehdunew = fits.BinTableHDU.from_columns([col])
-    new_columns = tablehdu.columns + tablehdunew.columns
-    tablehdu = fits.BinTableHDU.from_columns(new_columns)
-
-
-
-
-
-tablehdu.writeto(fitsloc, clobber = True)# initially makes file (not needed after 1st run)
-rawdata = fits.getdata(fitsloc,ext=1)
-print(rawdata.field(0))
-print(rawdata.field(1))
-print(rawdata.field(2))
+yout = y1highres-y2highres
+plt.plot(xhighres,yout)
+plt.show()
 
 
 
-    #wlim = len(normspec)
-    #plt.plot(wlen[0:wlim],normspec[0:wlim],label=specnames[specind][:20])
-    #plt.xlabel(r'$\lambda$ ($\mathrm{\AA}$)')
-    #plt.ylabel(r'$F$ $(10^{-17}$ ergs $s^{-1}cm^{-2}\mathrm{\AA}^{-1})$')
-    #plt.legend()
-#plt.show()
+
+gcredhsift = 2
+gclyalpha = 1215.67*(1+gcredhsift)
+normspeckstack = np.zeros(100000)
+
+for specind in specsample:
+    wlen, normspec, lyalpha = fitmeth.contfitv6(specind)
+    rfshift = gclyalpha - lyalpha
+    wlenshift = wlen + rfshift
+    wlenhighres = np.linspace(np.min(wlenshift), np.max(wlenshift), 100000)
+    wlenintpol = interpolate.interp1d(wlenshift, normspec, 'linear')
+    normspechighres = wlenintpol(wlenhighres)
+    normspeckstack = normspeckstack + normspechighres
+plt.figure()
+plt.plot(wlenhighres, normspeckstack)
+plt.show()
